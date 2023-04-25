87f986b1685b8f8140dcd2ea072f57680027234e
Archita Bhattad <87604491+ARCHITABHATTAD@users.noreply.github.com>
2023-04-25 23:45:15 +0530

    Add files via upload

---
+++ animation/6/can.ts
@@ -0,0 +1,59 @@
+var canvas:HTMLCanvasElement=<HTMLCanvasElement>document.getElementById("mycanvas");
+var context:CanvasRenderingContext2D=canvas.getContext("2d");
+var x=Math.random()*canvas.width;
+var y=Math.random()*canvas.height;
+var dx=(Math.random()-0.5)*8;
+var dy=(Math.random()-0.5)*8;
+var radius=30;
+function circle(x,y,dx,dy,radius){
+    this.x=x;
+    this.y=y;
+    this.dx=dx;
+    this.dy=dy;
+    this.radius=radius;
+
+    this.draw=function(){
+        context.beginPath();
+        context.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        context.strokeStyle="blue";
+        context.stroke();
+        context.fill();
+    }
+    this.update=function(){
+        
+        if(this.x+this.radius>canvas.width||this.x-this.radius<0){
+            this.dx=-this.dx;
+       }
+   
+       if(this.y+this.radius>canvas.height||this.y-this.radius<0){
+           this.dy=-this.dy;
+       }
+   
+       this.x+=this.dx;
+       this.y+=this.dy;
+       this.draw();
+       
+    }
+}
+
+var circleArray=[];
+
+for(var i=0;i<100;i++){
+    var radius=30;
+    var x=Math.random()*(canvas.width-radius*2)+radius;
+    var y=Math.random()*(canvas.height-radius*2)+radius;
+    var dx=(Math.random()-0.5)*8;
+    var dy=(Math.random()-0.5)*8;
+    
+    circleArray.push(new circle(x,y,dx,dy,radius));
+}
+
+
+function animate(){
+    requestAnimationFrame(animate);
+    context.clearRect(0,0,canvas.width,canvas.height);
+    for(var i=0;i<circleArray.length;i++){
+        circleArray[i].update();
+    }
+}
+animate();
